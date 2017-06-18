11e86c98b71ad55efe48c598580b6f4ec91fb960
Bobby Yang <codecheffy@gmail.com>
2017-06-17 21:10:39 -0700

    updated with mvp of lights control

--- console.py
+++ console.py
@@ -2,6 +2,9 @@ import cmd
 from pathlib import Path
 from models import config
 from models import bridge
+import requests
+import json
+
 
 class hue_house(cmd.Cmd):
     prompt = "(hue) "
@@ -20,5 +23,25 @@ class hue_house(cmd.Cmd):
 
     def do_emptyline(self, arg):
         return
+    
+    def do_lights(self, arg):
+        args = arg.split()
+        light_group = {
+                "livingroom" : "1",
+                "walkway" : "2",
+                "bedroom" : "3",
+                "outside" : "5"
+                }
+        body = {
+               "on" : True
+                }
+        
+        if args[1] == "off":
+            body["on"] = False
+
+        lights_url = self.bridge.url + self.bridge.username + "/groups/" + light_group.get(args[0]) + "/" + "action"
+        request = requests.put(lights_url, data=json.dumps(body))        
+
+
 if __name__ == "__main__":
     hue_house().cmdloop()
